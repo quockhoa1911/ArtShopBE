3c88d0100d33ed7c4dd2d58c140c4cc08deba202
Quynh Nguyen <quynh@stel.life>
2023-06-03 21:17:37 +0700

    update

--- api_base/pagination/base_pagination.py
+++ api_base/pagination/base_pagination.py
@@ -9,9 +9,9 @@ class Base_CustomPagination(PageNumberPagination):
 
     def get_paginated_response(self, data):
         return Response({
-            'links': {
-                'next': self.get_next_link(),
-                'previous': self.get_previous_link()
+            'page': {
+                'next': self.page.next_page_number(),
+                'previous': self.page.previous_page_number()
             },
             'total_all': self.page.paginator.count,
             'total_of_page': len(data),
