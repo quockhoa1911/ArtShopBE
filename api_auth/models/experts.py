087d16033a9b1db99d83ac2f37a845bc600a898a
Quynh Nguyen <quynh@stel.life>
2023-06-04 22:00:16 +0700

    update

--- api_auth/models/experts.py
+++ api_auth/models/experts.py
@@ -6,7 +6,7 @@ from api_auth.models import Role
 class Expert(BaseModel):
     name = models.CharField(max_length=256, blank=True, null=True)
     work_from = models.CharField(max_length=256, blank=True, null=True)
-    birthday = models.CharField(max_length=256, blank=True, null=True)
+    birthday = models.DateField(null=True, blank=True, default=None)
     role = models.ForeignKey(to=Role, on_delete=models.CASCADE, related_name="expert", default=None, null=True,
                              blank=True)
 
